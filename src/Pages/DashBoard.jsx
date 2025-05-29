3e5220c5e026efb21eba1136de60413767822187
rdunder <r.dunder@gmail.com>
2025-05-29 12:46:59 +0200

    p tag added just because

--- src/Pages/DashBoard.jsx
+++ src/Pages/DashBoard.jsx
@@ -4,6 +4,7 @@ export const DashBoard = () => {
   return (
     <section className='section-dashboard'>
         <h1>Dashboard</h1>
+        <p>Here the user will booked events and payment options</p>
     </section>
   )
 }
